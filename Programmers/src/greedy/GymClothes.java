2cde1189704db08bfc43087bf0bbf2f60d28a493
yenow <54181528+yenow@users.noreply.github.com>
2020-05-04 17:24:03 +0900

    Create GymClothes.java
    success

---
+++ Programmers/src/greedy/GymClothes.java
@@ -0,0 +1,59 @@
+package greedy;
+
+import java.rmi.Remote;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+	public int solution(int n, int[] lost, int[] reserve) {
+		int answer = n - lost.length;
+		ArrayList<Integer> lo = new ArrayList<Integer>();
+		ArrayList<Integer> re = new ArrayList<Integer>();
+		for (int a : lost) {
+			lo.add(a);
+		}
+		for (int a : reserve) {
+			re.add(a);
+		}
+		
+		for(int i=0; i< lo.size(); i++) {
+			for(int j=0; j< re.size(); j++) {
+				if(lo.get(i)==re.get(j)) {
+					lo.remove(i--);
+					re.remove(j--);
+					answer++;
+					break;
+				}
+			}
+		}
+		
+		while(lo.size()!=0) {
+			for(int i=0;i<re.size();i++) {
+				if(lo.get(0) == re.get(i)-1) {
+					re.remove(i);
+					answer++;
+				}else if(lo.get(0) == re.get(i)+1) {
+					re.remove(i);
+					answer++;
+				}
+			}
+			lo.remove(0);
+		}
+
+		return answer;
+	}
+}
+
+public class GymClothes {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int[] lost = { 3 };
+		int[] reserve = { 1 };
+		Solution s = new Solution();
+		System.out.println(s.solution(n, lost, reserve));
+	}
+
+}
